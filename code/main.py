39cf5fefc505e40609ae6a028f8c04a2681344c1
Josh Walker <joshwalker.oni@gmail.com>
2025-08-19 23:32:08 +0100

    Setup for the world

--- code/main.py
+++ code/main.py
@@ -1,5 +1,10 @@
-from settings import *
+from os.path import join
+from sys import exit
 
+import pygame
+from pytmx.util_pygame import load_pygame
+from settings import TILE_SIZE, WINDOW_HEIGHT, WINDOW_WIDTH
+from sprites import Sprite
 
 
 class Game:
@@ -7,6 +12,19 @@ class Game:
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('Tamers Gauntlet')
+
+        # groups
+        self.all_sprites = pygame.sprite.Group()
+
+        self.import_assets()
+        self.setup(self.tmx_maps['world'], 'house')
+
+    def import_assets(self):
+        self.tmx_maps = {'world': load_pygame(join('data', 'maps', 'world.tmx'))}
+
+    def setup(self, tmx_map, player_start_pos):
+        for x, y, surf in tmx_map.get_layer_by_name('Terrain').tiles():
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)
 
     def run(self):
         while True:
@@ -17,9 +35,10 @@ class Game:
                     exit()
 
             # game logic
+            self.all_sprites.draw(self.display_surface)
             pygame.display.update()
 
 
 if __name__ == '__main__':
     game = Game()
-
+    game.run()
