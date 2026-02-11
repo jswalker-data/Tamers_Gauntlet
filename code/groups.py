0a178830809ad6987abddb29f16b56144747282b
Josh Walker <joshwalker.oni@gmail.com>
2026-02-11 22:00:21 +0000

    working movement and black OUB

--- code/groups.py
+++ code/groups.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.math import Vector2 as vector
+from settings import WINDOW_HEIGHT, WINDOW_WIDTH
 
 
 # this is a copy of the in built sprite group
@@ -13,10 +14,10 @@ class AllSprites(pygame.sprite.Group):
 
     # need a custom draw method
     def draw(self, player_center):
-        self.offset.x = player_center[0]
-        self.offset.y = player_center[1]
-        print(player_center)
+        self.offset.x = player_center[0] - WINDOW_WIDTH / 2
+        self.offset.y = player_center[1] - WINDOW_HEIGHT / 2
         # return all sprites inside the group
         for sprite in self:
             # offset the image to the sprite
-            self.display_surface.blit(sprite.image, sprite.rect.topleft + self.offset)
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
