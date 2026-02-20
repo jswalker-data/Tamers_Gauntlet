2234f78b27444f126b33b904e2011d818b77044e
Josh Walker <joshwalker.oni@gmail.com>
2026-02-20 22:27:45 +0000

    proper animation speed

--- code/sprites.py
+++ code/sprites.py
@@ -1,5 +1,5 @@
 import pygame
-from settings import *
+from settings import ANIMATION_SPEED
 
 
 class Sprite(pygame.sprite.Sprite):
@@ -16,7 +16,7 @@ class AnimatedSprite(Sprite):
         super().__init__(pos, frames[self.frame_index], groups)
 
     def animate(self, dt):
-        self.frame_index += 4 * dt
+        self.frame_index += ANIMATION_SPEED * dt
         self.image = self.frames[int(self.frame_index % len(self.frames))]
 
     def update(self, dt):
